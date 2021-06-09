0b5b60b77bf69765cfbd1ca6159c52aff683608b
omarshaikh <omarshaikh@outlook.com>
2021-06-09 14:15:04 +0530

    debugged project

---
+++ debugexercise.py
@@ -0,0 +1,26 @@
+import random
+guess = ''
+while guess not in (0, 1):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+    if guess == "heads":
+        guess = 1
+    else:
+        guess = 0
+
+toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if guess == "heads":
+        guess = 1
+    else:
+        guess = 0
+
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
+
